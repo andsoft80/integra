92c9a6117559023e06c0a9b5c0c7f3552998d8f7
Саулин Андрей <a.saulin@Penoplex.net>
2020-07-20 18:08:43 +0300

    all put data

--- frontend/db_service/db_config.js
+++ frontend/db_service/db_config.js
@@ -1,7 +1,7 @@
 module.exports = {
     "user": "root", //default is sa
-    //"password": "Professional1",
-    "password": "root",
+    "password": "Professional1",
+    //"password": "root",
     "host": "localhost", // for local machine
     "database": "integra", // name of database
     
